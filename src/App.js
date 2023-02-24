01629492eb6c5c38671c6255c3b1f364c12ad1c3
kazune <mail_box-kazu@caffe-mocha.net>
2023-02-24 16:36:00 +0900

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { useState } from "react";
+import { useState , useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState(["Todo1","Todo2"]);
+  const [todos, setTodos] = useState
+  ([{id: 1 , name: "Todo1" , completed : false },
+]);
+
+const todoNameRef = useRef();
+
+const handleAddTodo=() =>{
+//タスクを追加する
+const name = todoNameRef.current.value;
+}
 
   return (
     <div className="App">
      <TodoList todos={todos}/>
-     <input type="text"/>
-     <button>タスクの追加</button>
+     <input type="text" ref={todoNameRef}/>
+     <button onClick={handleAddTodo}>タスクの追加</button>
      <button>完了したタスクの削除</button>
      <div>残りのタスク:0</div>
     </div>
   );
 }
+// 動画38:28
 
 export default App;
